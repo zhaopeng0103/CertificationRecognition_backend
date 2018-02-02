94f87436fc53b34ebeda2fd1a5b102bc44bbd735
pzhao <424107420@qq.com>
2018-02-03 00:41:32 +0800

    [Edit] 修改拦截器

--- src/main/java/com/peng/certrecognition/configuration/WebInterceptorConfiguration.java
+++ src/main/java/com/peng/certrecognition/configuration/WebInterceptorConfiguration.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebInterceptorConfiguration extends WebMvcConfigurerAdapter {
 
     private String[] excludePathPatterns = new String[]{
-            "/login",
-            "/register",
+            "/user/login",
+            "/user/register",
     };
 
     @Bean
